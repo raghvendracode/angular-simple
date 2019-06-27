924fac082fc5dd8f133dbd8033aed1069870f9d5
ttuser <raghvendra.singh@techtreeit.com>
2019-06-27 14:14:59 +0530

    message name

---
+++ .history/src/app/users/users.component_20190627125237.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {DataService} from '../data.service';
+import {Observable} from 'rxjs';
+
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss']
+})
+export class UsersComponent implements OnInit {
+
+  users$: object;
+total;
+total1;
+  constructor(private data: DataService) { }
+
+  ngOnInit() {
+    this.data.getUsers().subscribe(
+      (result) => {
+       // console.log(result);
+        this.users$ = result;
+        this.total = result;
+        this.total1 =  this.total.name;
+        console.log(this.total1);
+      }
+    );
+  }
+
+}
